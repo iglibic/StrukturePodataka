756ff656464e911b91cdbf8c9409652fd85c759c
iglibic <ivanglibic77@gmail.com>
2024-10-13 10:09:33 +0200

    Update FileName.cpp

--- Vjezba1/FileName.cpp
+++ Vjezba1/FileName.cpp
@@ -1,7 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define ERROR_OF 1
+#define MAX 1024
+
+
+int NumberOfRows(const char*);
 
 int main() {
+	 
+	int numOfRows = NumberOfRows("imedat.txt");
 
-	printf("Hello World.\n");
 	return 0;
+
+
+}
+
+int NumberOfRows(const char*) {
+	FILE *fp = NULL;
+	int numOfRows = 0;
+	char buffer[MAX] = {};
+	fp = fopen(fileName, "r");
+	if (!fp) {
+
+		return -1;
+	}
+	while (!feof(fp)) {
+		fgets(buffer, MAX, fp);
+		numOfRows++;
+	}
+
 }
