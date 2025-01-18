e513083c8f540f901c5dc3fde6033e06a623de97
iglibic <ivanglibic77@gmail.com>
2025-01-18 11:44:34 +0100

    Update FileName.c

--- Vjezba1/FileName.c
+++ Vjezba1/FileName.c
@@ -3,82 +3,81 @@
 #include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+#define FILE_NOT_OPENED (-1)
 #define MAX 1024
 #define MAX_POINTS 100
-#define FILE_NOT_OPENED (-1)
 
 typedef struct {
-    char name[MAX];
-    char surname[MAX];
-    int points;
-} Students;
+	char name[MAX];
+	char surname[MAX];
+	int points;
+}Students;
 
 int numOfRows(const char* fileName);
 Students* LoadStudents(const char* fileName, int numOfStudents);
 int printStudents(Students* students, int numOfStudents);
 
 int main() {
-    int totalStudents = numOfRows("Studenti.txt");
 
-    if (totalStudents == FILE_NOT_OPENED) {
-        printf("Error: Could not open the file!\n");
-        return FILE_NOT_OPENED;
-    }
+	int numberOfStudents = numOfRows("Studenti.txt");
+	
+	Students* students = LoadStudents("Studenti.txt", numberOfStudents);
+	if (students == NULL) {
+		printf("Error: Could not load students!");
+		return EXIT_FAILURE;
+	}
 
-    Students* students = LoadStudents("Studenti.txt", totalStudents);
-    if (students == NULL) {
-        printf("Error: Could not load students!\n");
-        return EXIT_FAILURE;
-    }
+	printStudents(students, numberOfStudents);
 
-    printStudents(students, totalStudents);
+	free(students);
 
-    free(students);
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
 int numOfRows(const char* fileName) {
-    FILE* fp = fopen(fileName, "r");
-    if (fp == NULL) {
-        return FILE_NOT_OPENED;
-    }
+	FILE* fp = fopen(fileName, "r");
+		if (fp == NULL) {
+			printf("Error: Could not open the file!");
+			return FILE_NOT_OPENED;
+		}
 
-    char buffer[MAX];
-    int totalStudents = 0;
+	int totalStudents = 0;
+	char buffer[MAX] = 0;
 
-    while (fgets(buffer, MAX, fp)) {
-        totalStudents++;
-    }
+	while (fgets(buffer, MAX, fp)) {
+		totalStudents++;
+	}
 
-    fclose(fp);
-    return totalStudents;
+	fclose(fp);
+
+	return totalStudents;
 }
 
 Students* LoadStudents(const char* fileName, int numOfStudents) {
-    FILE* fp = fopen(fileName, "r");
-    if (fp == NULL) {
-        return NULL;
-    }
+	FILE* fp = fopen(fileName, "r");
+		if (fp == NULL) {
+			printf("Error: Could not open the file!");
+			return NULL; //Vraća NULL jer funkcija ne vraća vrijednost nego pokazivač, pa ne može vratiti neku vrijednost
+		}
 
-    Students* students = (Students*)malloc(numOfStudents * sizeof(Students));
-    if (students == NULL) {
-        fclose(fp);
-        return NULL;
-    }
+	Students* students = (Students*)malloc(numOfStudents * sizeof(Students));
+	if (students == NULL) {
+		fclose(fp);
+		return NULL;
+	}
 
-    for (int i = 0; i < numOfStudents; i++) {
-        fscanf(fp, "%s %s %d", students[i].name, students[i].surname, &students[i].points);
-    }
-    fclose(fp);
-    return students;
+	for (int i = 0; i < numOfStudents; i++) {
+		fscanf(fp, "%s %s %d", students[i].name, students[i].surname, &students[i].points);
+	}
+
+	fclose(fp);
+
+	return students;
 }
 
 int printStudents(Students* students, int numOfStudents) {
-    for (int i = 0; i < numOfStudents; i++) {
-        float relativePoints = (float)students[i].points / MAX_POINTS * 100;
-        printf("Name: %s, Surname: %s, Points: %d, Relative points: %f\n",
-            students[i].name, students[i].surname, students[i].points, relativePoints);
-    }
-    return 0;
+	for (int i = 0; i < numOfStudents; i++) {
+		float relativePoints = (float)students[i].points / MAX_POINTS * 100;
+		printf("Name: %s %s, Points: %d, Relative points: %f", students[i].name, students[i].surname, students[i].points, relativePoints);
+	}
 }
